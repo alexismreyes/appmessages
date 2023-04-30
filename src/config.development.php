dd2aac46c0d3b5fb6632dbf78d58e5fb4206f686
Alexis <alexis_mreyes@hotmail.com>
2023-04-30 17:58:23 -0500

    Commit after remove sendsms.php

--- src/config.development.php
+++ src/config.development.php
@@ -17,7 +17,7 @@ return [
         "SERVER_PASSWORD" => "", // SMTP server password
     ],
     "JWT" => [
-        "SECRET_KEY" => "3wyp5eqT99X8Nv9I", // API Secret Key
+        "SECRET_KEY" => "5fv0I9PGtSebDOef", // API Secret Key
         "ALGORITHM" => "HS512", // API Algorithm
         "AUTH_HEADER" => "X-Authorization", // API Auth Header (Note: The "Authorization" header is removed by IIS, use "X-Authorization" instead.)
         "NOT_BEFORE_TIME" => 0, // API access time before login
